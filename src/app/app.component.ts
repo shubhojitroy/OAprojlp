b9a64eec9622570405d6b448abba52cb71a0fa76
shubhojitroy <royshubhojit@gmail.com>
2025-05-12 18:34:17 +1000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ContactComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'OAprojlp';
+  title = 'OA Landing Page';
 }
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SecBodyComponent } from './sec-body/sec-body.component';
+
+@Component({
+  selector: 'app-body',
+  imports: [SecBodyComponent],
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.scss',
+})
+export class BodyComponent {}
